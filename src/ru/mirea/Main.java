8c14f32a566319266301c341afdb43d68ebfd8e0
Artem <qewda>
2020-09-04 11:16:50 +0300

    Add from to do tasks

--- src/ru/mirea/Main.java
+++ src/ru/mirea/Main.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Scanner scanner = new Scanner(System.in);
+	// write your code her
+
+        ////////////////// Task1 /////////////
+       /* Scanner scanner = new Scanner(System.in);
 
         int a = 1; //4 байта
         float b = 2.2f; //8-16
@@ -25,7 +27,26 @@ public class Main {
         b = a;
         a= (int) b;
 
-        System.out.println(a);
+        System.out.println(a);*/
 
+        ////////////////// Task3 /////////////
+
+        int[] mass = {1,2,3,4,5,6,7};
+
+        int summ = 0;
+        for(int a: mass){
+            summ+=a;
+        }
+
+        System.out.println("Число суммы масства равно: "+summ );
+
+        ////////////////// Task4 /////////////
+
+        System.out.println((int)(returnRandInt(100)));
+    }
+
+
+    public static int returnRandInt(int scale){
+        return (int)(Math.random()*scale);
     }
 }
